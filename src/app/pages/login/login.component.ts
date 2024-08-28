7c1561a7bf6c08026c7500409a4e2451ce51b4df
EsloverDev <edwin.grow.code@gmail.com>
2024-08-27 19:50:56 -0500

    comportamiento del login

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,17 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   standalone: true,
-  imports: [ IonicModule ],
+  imports: [ IonicModule, ReactiveFormsModule ],
   styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent  implements OnInit {
 
-  constructor() { }
+  frmLogin: FormGroup;
 
+  constructor() {
+    this.frmLogin = new FormGroup({
+      user: new FormControl('edwin@mail.com', Validators.required),
+      pass: new FormControl('1234', Validators.required)
+    })
+  }
+
+  login(){
+    console.log(this.frmLogin.value.user);
+  }
   ngOnInit() {}
 
 }
